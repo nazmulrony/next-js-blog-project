342cc68bb6ec62ac6953d24eb42c187e0b697bc7
nazmulrony <nhrony65@gmail.com>
2023-06-26 16:39:37 +0600

    button modified

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -2,22 +2,22 @@ import Link from 'next/link';
 import { FC, ReactNode } from 'react';
 
 type ButtonProps = {
-    children: ReactNode;
-    url?: string;
+	children: ReactNode;
+	url?: string;
 };
 
 const Button: FC<ButtonProps> = ({ children, url }) => {
-    return url ? (
-        <Link href={url}>
-            <button className="px-5 py-3 cursor-pointer bg-[#53c28b] border-none rounded w-max text-white">
-                {children}
-            </button>
-        </Link>
-    ) : (
-        <button className="px-5 py-3 cursor-pointer bg-[#53c28b] border-none rounded w-max text-white">
-            {children}
-        </button>
-    );
+	return url ? (
+		<Link href={url}>
+			<button className="px-5 py-3 cursor-pointer bg-[#53c28b] border-none rounded w-max text-white hover:bg-[#46a877] duration-200">
+				{children}
+			</button>
+		</Link>
+	) : (
+		<button className="px-5 py-3 cursor-pointer bg-[#53c28b] border-none rounded w-max text-white hover:bg-[#53c28b] duration-200">
+			{children}
+		</button>
+	);
 };
 
 export default Button;
